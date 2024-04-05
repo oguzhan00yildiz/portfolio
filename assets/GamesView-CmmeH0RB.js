919f83ab41a6bd62c9d4823f8cbdc3341d9becdf
Taha ÇEKEN <taha-ceken@hotmail.com>
2024-04-05 23:46:50 +0300

    Updates

--- assets/GamesView-CmmeH0RB.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,o as r,c as s,t}from"./index-DBPEXfMa.js";const o={};function c(e,n){return r(),s("div",null," Games "+t(e.$route.params.id),1)}const _=a(o,[["render",c]]);export{_ as default};
